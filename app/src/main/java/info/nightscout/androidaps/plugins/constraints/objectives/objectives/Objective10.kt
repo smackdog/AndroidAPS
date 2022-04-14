c3cf4a95e8e08557e9acc489e1049e1a984d53a4
smackdog <55664992+smackdog@users.noreply.github.com>
2022-04-13 18:58:27 -0600

    Update Objective10.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective10.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective10.kt
@@ -7,6 +7,6 @@ import info.nightscout.androidaps.utils.T
 class Objective10(injector: HasAndroidInjector) : Objective(injector, "auto", R.string.objectives_auto_objective, R.string.objectives_auto_gate) {
 
     init {
-        tasks.add(MinimumDurationTask(this, T.minutes(2).msecs()))
+        tasks.add(MinimumDurationTask(this, T.minutes(2)))
     }
 }
